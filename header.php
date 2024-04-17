8f7cb643821a1309112cc98ce836030dc8094bf0
Kenta <kd1326756@st.kobedenshi.ac.jp>
2024-04-17 11:59:06 +0900

    header

---
+++ header.php
@@ -0,0 +1,29 @@
+<div id="hamberger">
+    <nav>
+        <ul>
+            <h3>KOBE DENSHI</h3>
+            <li><a href="home.php">HOME</a></li>
+            <li><a href="mypage.php">PROFILE</a></li>
+            <li><a href="QandA.php">Q % A</a></li>
+            <li><a href="puestion.php">QUENTION</a></li>
+            <li><a href="post.php">POST</a></li>
+        </ul>
+        <button onclick="location.href='logout.php'">LOGOUT
+        </button>
+    </nav>
+    <div class="toggle-btn">
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+
+    <div id="mask"></div>
+</div>
+<header>
+    <a id="header" href="index.php">
+        <h1>KOBE DENSHI</h1>
+    </a>
+    <button id="user" onclick="location.href='loginform.php'">LOGIN
+    </button>
+    <div id="menuarea"></div>
+</header>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <link rel="stylesheet" href="css/footer.css">
+</head>
+
+<?php
+include "header.php";
+?>
+
+<body>
+
+    <script src="script/header.js"></script>
+
+</body>
+
+</html>
