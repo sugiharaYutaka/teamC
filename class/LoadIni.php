cbb7b61aad33d86bb33cb1d85756ce86c6c15ce3
Yutaka Sugihara <kd1336505@st.kobedenshi.ac.jp>
2024-04-19 15:41:54 +0900

    [update] iniファイルからDBユーザーの情報取得

---
+++ class/LoadIni.php
@@ -0,0 +1,33 @@
+<?php
+class LoadIni
+{
+
+    private $ini_data;
+    public $DATABASE_USERNAME;
+    public $DATABASE_PASSWORD;
+    public $DATABASE_NAME;
+    public function __construct()
+    {
+        $this->ini_data = parse_ini_file(__DIR__ . '/../env.ini', true);
+        $this->DATABASE_USERNAME = $this->ini_data['database']['DATABASE_USERNAME'];
+        $this->DATABASE_PASSWORD = $this->ini_data['database']['DATABASE_PASSWORD'];
+        $this->DATABASE_NAME = $this->ini_data['database']['DATABASE_NAME'];
+    }
+    public function get($index)
+    {
+        ///DATABASE_USERNAME
+        //DATABASE_PASSWORD
+        //DATABASE_NAME
+        return $this->ini_data[$index];
+
+    }
+    public function getAll()
+    {
+        ///DATABASE_USERNAME
+        //DATABASE_PASSWORD
+        //DATABASE_NAME
+        return $this->ini_data;
+
+    }
+}
+?>
