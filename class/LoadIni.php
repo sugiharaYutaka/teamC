157c6c936912e7226b8125e5967069105ccaeea5
Yutaka Sugihara <kd1336505@st.kobedenshi.ac.jp>
2024-04-19 15:44:33 +0900

    []update

--- class/LoadIni.php
+++ class/LoadIni.php
@@ -8,7 +8,7 @@ class LoadIni
     public $DATABASE_NAME;
     public function __construct()
     {
-        $this->ini_data = parse_ini_file(__DIR__ . '/../env.ini', true);
+        $this->ini_data = parse_ini_file(__DIR__ . '/../setting/env.ini', true);
         $this->DATABASE_USERNAME = $this->ini_data['database']['DATABASE_USERNAME'];
         $this->DATABASE_PASSWORD = $this->ini_data['database']['DATABASE_PASSWORD'];
         $this->DATABASE_NAME = $this->ini_data['database']['DATABASE_NAME'];
