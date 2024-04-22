e3caa21fe71212b6fe86f29c34dc4b850305c03b
Kenta <kd1326756@st.kobedenshi.ac.jp>
2024-04-22 12:08:02 +0900

    passwordconfirm

--- signup_form.php
+++ signup_form.php
@@ -26,20 +26,16 @@ include 'header.php';
 <link href="css/form.css" rel="stylesheet">
 <script type="text/javascript">
     function check() {
-        var check1 = document.form2.username.value;
-        var check2 = document.form2.userid.value;
-        var check3 = document.form2.pw1.value;
-        var check4 = document.form2.pw2.value;
-        check5 = document.form2.confirm.checked;
+        var check1 = document.form.name.value;
+        var check2 = document.form.email.value;
+        var check3 = document.form.pw1.value;
+        var check4 = document.form.pw2.value;
 
         if (check1 == "" || check2 == "" || check3 == "" || check4 == "") {
             document.getElementById('alert').innerHTML = "<p>全項目入力必須です。入力してください。</p>";
             return false;
         } else if (check3 != check4) {
             document.getElementById('alert').innerHTML = "<p>確認用パスワードが異なります。</p>";
-            return false;
-        } else if (check5 == false) {
-            document.getElementById('alert').innerHTML = "<p>入力内容を確認してチェックを入れてください。</p>";
             return false;
         } else {
             return true;
@@ -53,26 +49,26 @@ include 'header.php';
             <p><?php echo $login_err; ?></p>
         <?php endif; ?>
     </div>
-    <form action="register.php" method="POST">
+    <form action="register.php" method="POST" name="form">
         <div class="row">
             <span class="label">お名前</span>
-            <input type="text" name="name" placeholder="お名前" required="required">
+            <input type="text" id="name" name="name" placeholder="お名前" required="required">
         </div>
         <div class="row">
             <span class="label">メールアドレス</span>
-            <input type="email" name="email" placeholder="メールアドレス" required="required">
+            <input type="email" id="email" name="email" placeholder="メールアドレス" required="required">
         </div>
         <div class="row">
             <span class="label">パスワード</span>
-            <input type="password" name="password" placeholder="パスワード" required="required">
+            <input type="password" id="pw1" name="password" placeholder="パスワード" required="required">
         </div>
         <div class="row">
             <span class="label">パスワード確認用</span>
             <p>※同じパスワードをもう一度入力してください</p>
-            <input type="password" name="password" placeholder="パスワード確認用" required="required">
+            <input type="password" id="pw2" name="password" placeholder="パスワード確認用" required="required">
         </div>
         <div class="row">
-            <button type="submit" class="btn">登録</button>
+            <input type="submit" class="btn" onclick="return check()" value="登録">
     </form>
 </div>
 <div class="button">
