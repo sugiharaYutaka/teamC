46315418e3f316e7e58ae55ddabfe6901d55f763
Kenta <kd1326756@st.kobedenshi.ac.jp>
2024-04-22 11:57:40 +0900

    passwordconfirm

--- signup_form.php
+++ signup_form.php
@@ -24,6 +24,28 @@ unset($_SESSION['login_err']);
 include 'header.php';
 ?>
 <link href="css/form.css" rel="stylesheet">
+<script type="text/javascript">
+    function check() {
+        var check1 = document.form2.username.value;
+        var check2 = document.form2.userid.value;
+        var check3 = document.form2.pw1.value;
+        var check4 = document.form2.pw2.value;
+        check5 = document.form2.confirm.checked;
+
+        if (check1 == "" || check2 == "" || check3 == "" || check4 == "") {
+            document.getElementById('alert').innerHTML = "<p>全項目入力必須です。入力してください。</p>";
+            return false;
+        } else if (check3 != check4) {
+            document.getElementById('alert').innerHTML = "<p>確認用パスワードが異なります。</p>";
+            return false;
+        } else if (check5 == false) {
+            document.getElementById('alert').innerHTML = "<p>入力内容を確認してチェックを入れてください。</p>";
+            return false;
+        } else {
+            return true;
+        }
+    }
+</script>
 <div class="main-container margin-top">
     <h2 class="center">アカウント新規作成</h2>
     <div id="alert">
