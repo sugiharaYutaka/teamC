1ab28b8ade0315fb80957df3c8063828579ab4f6
tachibanajun <st1329537kd1329537@st.kobedenshi.ac.jp>
2024-04-23 09:26:59 +0900

    [add]mypage

--- mypage.php
+++ mypage.php
@@ -6,7 +6,7 @@ include "header.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="mypage.css">
+    <link rel="stylesheet" href="css/mypage.css">
     <title>mypage</title>
 </head>
 <body>
