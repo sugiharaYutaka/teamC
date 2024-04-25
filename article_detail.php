c1ae6bcd1655e36504e4df3fa43cfd43e529c93b
Yutaka Sugihara <kd1336505@st.kobedenshi.ac.jp>
2024-04-25 15:04:29 +0900

    [update]

---
+++ article_detail.php
@@ -0,0 +1,49 @@
+<?php
+include "header.php";
+?>
+<link href="css/article_detail.css" rel="stylesheet">
+<div class="main-container margin-top">
+    <div class="content-group">
+        <div class="start-content">
+            <div class="row">
+                <div class="w-20">
+                    <div class="icon-wrap" alt="icon">
+                        <img src="" class="user-icon" onError="this.onerror=null;this.src='../teamC/img/user_icon.png'">
+                    </div>
+                </div>
+                <div class="w-80">
+                    <span class="title">
+                        神戸太郎
+                    </span>
+                </div>
+                <div class="tag-container">
+                    <span class="tag">AWS</span>
+                    <span class="tag">Linux</span>
+                    <span class="tag">Python</span>
+                </div>
+            </div>
+            <div class="row">
+                <hr>
+                <div class="article-title">
+                    <span>AWSでブロックチェーン実装してみた</span>
+                </div>
+            </div>
+            <div class="row">
+                <span class="main-text-title">
+                    1.はじめに
+                </span>
+                <hr>
+                <span class="main-text">
+                    きょうはきょうはきょうあｈ
+                </span>
+
+            </div>
+
+        </div>
+        <div class="end-content">
+            <div class="row">
+
+            </div>
+        </div>
+    </div>
+</div>
