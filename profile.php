825450860e2ede733fe573aa7977c178cb029d25
Kenta <kd1326756@st.kobedenshi.ac.jp>
2024-04-25 13:30:22 +0900

    profilephpadd

--- profile.php
+++ profile.php
@@ -9,7 +9,7 @@ include "header.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/mypage.css">
     <link rel="stylesheet" href="css/header.css">
-    <title>mypage</title>
+    <title>PROFILE</title>
 </head>
 
 <body>
@@ -19,34 +19,21 @@ include "header.php";
                 <div class="icon">プロフィール画像</div>
                 <img src="img/user_icon.png" id="iconimg">
                 <label class="file_label">
-                    <div class="icon-change">
-                        <form>
-                            <input type="file" name="file">画像変更
-                        </form>
-                    </div>
                 </label>
             </div>
             <div class="right-contents">
                 <div class="name">名前</div>
-                <input type="text" class="textBoxEl">
+                <p class="username">さかもと</p>
                 <div class="achievements">
                     <input type="button" value="achievements" class="buttonEl">
                 </div>
             </div>
             <div class="main-block-wrapper2">
-                <div class="mailaddress">メールアドレス</div><form><input type="email" class="textBoxEl"></form>
-                <div class="password">パスワード</div><form><input type="password" class="textBoxEl"></form>
-                <div class="question">自分の質問</div>
-                <div class="question">自分の記事</div>
+                <div class="question">さかもとさんの質問</div>
+                <div class="question">さかもとさんの記事</div>
             </div>
         </div>
     </div>
-    <footer>
-        <button onclick="location.href='home_H.php'">ホームに戻る</button>
-        <form><input type="submit" form="memberdata" value="保存">
-        </form>
-    </footer>
-
 </body>
 
 </html>
