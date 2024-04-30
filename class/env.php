b2e399987b4ef1789b15745b708e6cebb6c00767
NAKAI ZEN <kd1293049@st.kobedenshi.ac.jp>
2024-04-30 14:00:05 +0900

    docker

--- class/env.php
+++ class/env.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'db');
 define('DB_NAME', 'teamc');
-define('DB_USER', 'kobe');
-define('DB_PASS', 'denshi');
+define('DB_USER', 'kd');
+define('DB_PASS', 'kd');
