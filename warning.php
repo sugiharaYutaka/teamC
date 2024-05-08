ccf29b0f89d39ba70636b8ceeacf5215b3594995
tachibanajun <st1329537kd1329537@st.kobedenshi.ac.jp>
2024-05-08 10:34:37 +0900

    [add]warningpage

---
+++ warning.php
@@ -0,0 +1,12 @@
+<?php
+    include "header.php";
+?>
+<link rel="stylesheet" href="css/warning.css">
+
+<div class="main-container margin-top">
+    <div class="message-container">
+        <p>ログインしていません</p>
+        <a href="login.php" class="flex-item">ログインする</a>
+        <a href="#" onclick="history.back(); return false;" class="flex-item">記事に戻る</a>
+    </div>
+</div>
