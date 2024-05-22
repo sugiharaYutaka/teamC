d8555bbe6de91de243f3c60b0b7c04e82a9b0215
NAKAI ZEN <kd1293049@st.kobedenshi.ac.jp>
2024-05-22 12:07:03 +0900

    teamc

--- answer.php
+++ answer.php
@@ -1,7 +1,7 @@
 <?php
 include "header.php";
-require_once ('../teamC/class/getQA.php');
-require_once ('../teamC/class/UserLogic.php');
+require_once('class/getQA.php');
+require_once('class/UserLogic.php');
 $userLogic = new UserLogic();
 $user = $userLogic->getUserById($question['user_id']);
 
@@ -27,16 +27,14 @@ if ($result) {
 
 <div class="main-container margin-top">
     <?php if ($user_id != 1) { ?>
-        <form class="answer-form" method="post" action="../teamC/class/sendAnswer.php?user_id=<?= $user_id ?>"
-            enctype="multipart/form-data">
+        <form class="answer-form" method="post" action="class/sendAnswer.php?user_id=<?= $user_id ?>" enctype="multipart/form-data">
         <?php } else { ?>
-            <form class="answer-form" method="post" action="../teamC/warning.php" enctype="multipart/form-data">
+            <form class="answer-form" method="post" action="warning.php" enctype="multipart/form-data">
             <?php } ?>
             <div class="row">
                 <div class="user-data">
                     <div class="w-20">
-                        <img src="img/<?php echo $user['icon_filename'] ?>" class="user-icon"
-                            onError="this.onerror=null;this.src='../teamC/img/user_icon.png'">
+                        <img src="img/<?php echo $user['icon_filename'] ?>" class="user-icon" onError="this.onerror=null;this.src='img/user_icon.png'">
                     </div>
                     <div class="w-80">
                         <span class="name">
@@ -63,16 +61,16 @@ if ($result) {
                     <button type="submit" class="btn">送信</button>
                 </div>
             </div>
-        </form>
-        <hr>
-        <span class="label title-margin">回答一覧</span>
-        <?php
-        foreach ($answers as $answer) {
-            echo '
+            </form>
+            <hr>
+            <span class="label title-margin">回答一覧</span>
+            <?php
+            foreach ($answers as $answer) {
+                echo '
         <div class="row-answer">
             <div class="w-20">
                 <div class="icon-wrap" alt="icon">
-                    <img src="img/' . $answer['icon_filename'] . '" class="user-icon" onError="this.onerror=null;this.src=\'../teamC/img/user_icon.png\'">
+                    <img src="img/' . $answer['icon_filename'] . '" class="user-icon" onError="this.onerror=null;this.src=\'img/user_icon.png\'">
                 </div>
                 <div class="name-wrap">
                     <span class="user-name">
@@ -80,22 +78,22 @@ if ($result) {
                     </span>
                 </div>
             </div>';
-            if ($answer['bestans'] == 1) {
-                echo '<div class="bestw-80">
+                if ($answer['bestans'] == 1) {
+                    echo '<div class="bestw-80">
                         <i class="fa-solid fa-medal"></i>
                         <div class"bestText-container">
                         <div>ベストアンサー</div>';
-            } else {
-                echo '<div class="w-80">';
-            }
-            echo '<div class="text-wrap">
+                } else {
+                    echo '<div class="w-80">';
+                }
+                echo '<div class="text-wrap">
                     <span class="answer-text">
                         ' . htmlspecialchars($answer['text']) . '
                     </span>
                 </div>';
-            if ($answer['bestans'] == 1)
-                echo '</div>';
-            echo '<form method="POST" action="bestanswer.php?question_id=' . $answer['quetion_id'] . '">
+                if ($answer['bestans'] == 1)
+                    echo '</div>';
+                echo '<form method="POST" action="bestanswer.php?question_id=' . $answer['quetion_id'] . '">
                     <div class="bot-wrap">
                 </form>
             </div>
@@ -103,6 +101,6 @@ if ($result) {
         </div>
         <hr>
         ';
-        }
-        ?>
+            }
+            ?>
 </div>
